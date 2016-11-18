3f8b7e7cad7b32bf855d5c5b0a40806822d3d305
Kevin A. Finley <kf.two.two@gmail.com>
2016-11-18 08:04:42 -0800

    it will add first spec and pass

---
+++ lib/word_count.rb
@@ -0,0 +1,5 @@
+class String
+  define_method(:word_counter) do |number|
+    ["h"]
+  end
+end
---
+++ spec/word_count_spec.rb
@@ -0,0 +1,17 @@
+require('rspec')
+require('word_count')
+
+describe('Sting#word_counter') do
+  it "will return an array" do
+    expect("".word_counter(1)).to be_an(Array)
+  end
+  # it "will return 'h' in the array" do
+  #   expect("h".word_counter(1)).to include(String)
+  # end
+  # it "will return 'h' in the array" do
+  #   expect("h".word_counter(1)).to(eq(["h"]))
+  # end
+  # it "will take an argument for the word" do
+  #   expect("ha".word_counter(2)).to(eq(["h"]))
+  # end
+end
