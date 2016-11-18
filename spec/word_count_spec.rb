16c62923fa1442c4dc9867836228243c93794c97
Kevin A. Finley <kf.two.two@gmail.com>
2016-11-18 08:05:02 -0800

    it will add second spec and pass

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,9 +5,9 @@ describe('Sting#word_counter') do
   it "will return an array" do
     expect("".word_counter(1)).to be_an(Array)
   end
-  # it "will return 'h' in the array" do
-  #   expect("h".word_counter(1)).to include(String)
-  # end
+  it "will return 'h' in the array" do
+    expect("h".word_counter(1)).to include(String)
+  end
   # it "will return 'h' in the array" do
   #   expect("h".word_counter(1)).to(eq(["h"]))
   # end
